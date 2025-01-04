1d61b2488bfd5fa8ffc5ad3ddf2169dbdcba001e
judo2000 <judo2000@comcast.net>
2025-01-04 08:08:50 -0700

    Add google font barlow to project

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,9 @@ export default function Home() {
   return (
     <div>
       <h1 className="font-bold text-blue-500">Welcome to the ccourse</h1>
+      <h1 className="font-bold text-blue-500 font-barlow">
+        Welcome to the ccourse
+      </h1>
     </div>
   );
 }
