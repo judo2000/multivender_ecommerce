331454d9db2f53efc3b1bdb29eed8e3a90c8bc79
judo2000 <judo2000@comcast.net>
2025-01-05 07:50:56 -0700

    Install primsa and create User table in our mysql database

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,6 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-export default clerkMiddleware(async (auth, req, next) => {
+export default clerkMiddleware(async (auth, req) => {
   const isProtectedRoute = createRouteMatcher(["/dashboard(.*)"]);
   if (isProtectedRoute(req)) await auth.protect();
 });
